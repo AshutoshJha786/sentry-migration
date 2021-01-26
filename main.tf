52d763dac13111b500c70534e343732cb2767cdb
Ashutosh Jha <50641824+AshutoshJha786@users.noreply.github.com>
2021-01-26 14:59:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,15 +37,9 @@ resource "aws_security_group" "sentry-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
